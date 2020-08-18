ed98626c715569ac9ec9003beaf686766ad16625
xSimplez <69173408+xSimplez@users.noreply.github.com>
2020-08-18 23:05:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./Storage/config.json");
-const packages = require("./package.js")
+const packages = require("./package.json")
 const bot = new Discord.Client({
     disableEveryone: true,
     autoReconnect: true,
